2b216a00c0e5eaa457102e8d43f6ca3f7e9b0bce
Mohammad Salim <mohammadsalim017427@gmail.com>
2025-01-21 23:37:17 +0600

    refactor routes with util function

--- src/routes/admin.routes.tsx
+++ src/routes/admin.routes.tsx
@@ -5,16 +5,16 @@ import CreateFaculty from "../pages/admin/CreateFaculty";
 import CreateStudent from "../pages/admin/CreateStudent";
 import { NavLink } from "react-router-dom";
 
-type TRoute = {
-    path:string;
-    element:ReactNode
-}
+// type TRoute = {
+//     path:string;
+//     element:ReactNode
+// }
 type TSidebarItem = {
     key:string;
     label:ReactNode;
     children?: TSidebarItem[]
 };
-const adminPaths = [
+export const adminPaths = [
   {
     name: "Dashboard",
     path: "dashboard",
@@ -72,26 +72,26 @@ const adminPaths = [
   )
    //programatical way
 
-    export const adminRoutes = adminPaths.reduce((acc:TRoute[],item)=>{
-        if(item.path && item.element)
-        {
-            acc.push({
-                path: item.path,
-                element: item.element
-            });
-        }
+    // export const adminRoutes = adminPaths.reduce((acc:TRoute[],item)=>{
+    //     if(item.path && item.element)
+    //     {
+    //         acc.push({
+    //             path: item.path,
+    //             element: item.element
+    //         });
+    //     }
 
-        if(item.children){
-            item.children.forEach((child)=>{
-                acc.push({
-                    path:child.path,
-                    element:child.element
-                });
-            });
-        }
-        console.log(acc);
-        return acc;
-    }, [])
+    //     if(item.children){
+    //         item.children.forEach((child)=>{
+    //             acc.push({
+    //                 path:child.path,
+    //                 element:child.element
+    //             });
+    //         });
+    //     }
+    //     console.log(acc);
+    //     return acc;
+    // }, [])
 
 //Hard coded way
 // export const adminPaths = [
---
+++ src/utils/routesGenerator.ts
@@ -0,0 +1,31 @@
+import { TRoute, TUserPath } from "../types/sidebar.type";
+
+// type TUserPath={
+//     name: string,
+//     path?: string,
+//     element?: ReactNode,
+//     children?: TUserPath[]
+// }
+export const routeGenerator = (items:TUserPath[])=>{
+       const routes = items.reduce((acc:TRoute[],item)=>{
+            if(item.path && item.element)
+            {
+                acc.push({
+                    path: item.path,
+                    element: item.element
+                });
+            }
+    
+            if(item.children){
+                item.children.forEach((child)=>{
+                    acc.push({
+                        path:child.path!,
+                        element:child.element
+                    });
+                });
+            }
+            console.log(acc);
+            return acc;
+        }, []);
+        return routes;
+}
