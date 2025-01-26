9b5fb4050edcddfea7060e49ca507a2e1d8312f8
Mohammad Salim <mohammadsalim017427@gmail.com>
2025-01-26 20:56:33 +0600

    login redirect and logOut

--- src/main.tsx
+++ src/main.tsx
@@ -7,12 +7,14 @@ import router from './routes/routes.tsx'
 import {Provider} from 'react-redux';
 import { persistor, store } from './redux/store.ts'
 import { PersistGate } from 'redux-persist/integration/react'
+import { Toaster } from 'sonner'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
     <RouterProvider  router={router}/>
     </PersistGate>
+    <Toaster expand visibleToasts={1}/>
     </Provider>
   </StrictMode>,
 )
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,29 +1,39 @@
 import { Button } from "antd";
-import { useForm } from "react-hook-form";
+import { FieldValues, useForm } from "react-hook-form";
 import { useLoginMutation } from "../redux/features/auth/authApi";
 import { useAppDispatch } from "../redux/hooks";
-import { setUser } from "../redux/features/auth/authSlice";
+import { setUser, TUser } from "../redux/features/auth/authSlice";
 import { verifyToken } from "../utils/verifyToken";
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
 
 const Login = () =>{
     const dispatch = useAppDispatch();
+    const navigate = useNavigate();
     const { register, handleSubmit } = useForm({
         defaultValues: {
           userId: 'A-0002',
           password: 'admin123',
         },
       });
-    const [login, { error}] = useLoginMutation();
-   
-    const onSubmit = async (data)=>{
-       const userInfo = {
-        id : data.userId,
-        password: data.password
-       };
-      const res = await login(userInfo).unwrap();
-      const user = verifyToken(res.data.accessToken);
-      console.log(user);
-      dispatch(setUser({ user: user, token: res.data.accessToken}))
+    const [login, { error}] = useLoginMutation();  
+    
+    const onSubmit = async (data: FieldValues)=>{
+      const toastId = toast.loading('Logging in');
+      try{
+        const userInfo = {
+          id : data.userId,
+          password: data.password
+         };
+        const res = await login(userInfo).unwrap();
+        const user = verifyToken(res.data.accessToken) as TUser;
+        console.log(user);
+        dispatch(setUser({ user: user, token: res.data.accessToken}));
+        toast.success('Logged in', {id: toastId , duration: 2000})
+        navigate(`/${user.role}/dashboard`)
+      } catch(err){
+        toast.error('Something went wrong', {id:toastId , duration: 2000})
+      }
     }
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
