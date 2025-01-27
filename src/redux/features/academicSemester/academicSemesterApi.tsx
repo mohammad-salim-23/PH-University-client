a9781e0bbda1c6cae24fdcddbca6b60c4eccc570
Mohammad Salim <mohammadsalim017427@gmail.com>
2025-01-27 12:09:10 +0600

    setting uup baseQuery and handle verify JWT error

---
+++ src/redux/features/academicSemester/academicSemesterApi.tsx
@@ -0,0 +1,13 @@
+import { baseApi } from "../../api/baseapi";
+
+const academicSemesterApi = baseApi.injectEndpoints({
+    endpoints: (builder) =>({
+        getAllSemesters: builder.query({
+            query: ()=>({
+                url: '/academic-semesters',
+                method:'GET'
+            })
+        })
+    })
+})
+export const {useGetAllSemestersQuery} = academicSemesterApi;
