2e58443a6953bc2b559507b9a38931d6cc7ac91c
Mohammad Salim <mohammadsalim017427@gmail.com>
2025-01-29 00:03:54 +0600

    typescript error fixed

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,5 @@
-import { Button } from "antd";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Button, Row } from "antd";
 import { FieldValues, useForm } from "react-hook-form";
 import { useLoginMutation } from "../redux/features/auth/authApi";
 import { useAppDispatch } from "../redux/hooks";
@@ -6,47 +7,54 @@ import { setUser, TUser } from "../redux/features/auth/authSlice";
 import { verifyToken } from "../utils/verifyToken";
 import { useNavigate } from "react-router-dom";
 import { toast } from "sonner";
+import PHForm from "../components/form/PHForm";
+import PHInput from "../components/form/PHInput";
+import Password from "antd/es/input/Password";
+const Login = () => {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  // const { register, handleSubmit } = useForm({
+  //   defaultValues: {
+  //     userId: "A-0002",
+  //     password: "admin123",
+  //   },
+  // });
+  const defaultValues = {
+    userId : 'A-0002',
+    password:'admin123'
+  }
+  const [login] = useLoginMutation();
 
-const Login = () =>{
-    const dispatch = useAppDispatch();
-    const navigate = useNavigate();
-    const { register, handleSubmit } = useForm({
-        defaultValues: {
-          userId: 'A-0002',
-          password: 'admin123',
-        },
-      });
-    const [login, { error}] = useLoginMutation();  
-    
-    const onSubmit = async (data: FieldValues)=>{
-      const toastId = toast.loading('Logging in');
-      try{
-        const userInfo = {
-          id : data.userId,
-          password: data.password
-         };
-        const res = await login(userInfo).unwrap();
-        const user = verifyToken(res.data.accessToken) as TUser;
-        console.log(user);
-        dispatch(setUser({ user: user, token: res.data.accessToken}));
-        toast.success('Logged in', {id: toastId , duration: 2000})
-        navigate(`/${user.role}/dashboard`)
-      } catch(err){
-        toast.error('Something went wrong', {id:toastId , duration: 2000})
-      }
+  const onSubmit = async (data: FieldValues) => {
+    console.log(data);
+    const toastId = toast.loading('Logging in');
+    try{
+      const userInfo = {
+        id : data.userId,
+        password: data.password
+       };
+      const res = await login(userInfo).unwrap();
+      const user = verifyToken(res.data.accessToken) as TUser;
+      console.log(user);
+      dispatch(setUser({ user: user, token: res.data.accessToken}));
+      toast.success('Logged in', {id: toastId , duration: 2000})
+      navigate(`/${user.role}/dashboard`)
+    } catch(err){
+      toast.error('Something went wrong', {id:toastId , duration: 2000})
     }
-    return (
-        <form onSubmit={handleSubmit(onSubmit)}>
+  };
+  return (
+  <Row  justify="center" align="middle" style={{height: "100vh"}}>
+      <PHForm onSubmit={onSubmit} defaultValues={defaultValues}>
       <div>
-        <label htmlFor="id">ID: </label>
-        <input type="text" id="id" {...register('userId')}/>
+        <PHInput type="text" name="userId" label="ID:" />
       </div>
-       <div>
-        <label htmlFor="password">Password: </label>
-        <input type="text" id="password" {...register('password')}/>
-       </div>
-       <Button htmlType="submit">Login</Button>
-        </form>
-    )
-}
+      <div>
+        <PHInput type="text" name="password" label="password" />
+      </div>
+      <Button htmlType="submit">Login</Button>
+    </PHForm>
+  </Row>
+  );
+};
 export default Login;
--- src/redux/api/baseapi.ts
+++ src/redux/api/baseapi.ts
@@ -1,5 +1,5 @@
 
-import { createApi, fetchBaseQuery, } from "@reduxjs/toolkit/query/react";
+import { BaseQueryApi, BaseQueryFn, createApi, DefinitionType, FetchArgs, fetchBaseQuery, } from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
 import { logout, setUser } from "../features/auth/authSlice";
 const baseQuery = fetchBaseQuery({
@@ -15,7 +15,7 @@ const baseQuery = fetchBaseQuery({
     }
 });
 //Customizing queries with baseQuery (Redux)
- const baseQueryWithRefreshToken = async( args , api , extraOptions)=>{
+ const baseQueryWithRefreshToken : BaseQueryFn<FetchArgs, BaseQueryApi, DefinitionType>  = async( args , api , extraOptions) : Promise<any>=>{
     let result = await baseQuery(args , api , extraOptions);
     console.log(result);
     if(result?.error?.status === 401){
